a36b2d0c6aa2b91c49f61534b419812179d385c6
tiagosantos-dev <tiagoltj@hotmail.com>
2021-05-19 09:23:23 -0300

    Criando serviços e implementação

---
+++ src/main/java/com/desafio/unifiscal/services/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.desafio.unifiscal.services.impl;
+
+import java.util.Optional;
+
+import com.desafio.unifiscal.services.UserService;
+import com.desafio.unifiscal.services.dto.UserDTO;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDTO save(UserDTO userDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<UserDTO> findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<UserDTO> findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
